b95f727d5d064288a3200c3bff1d49c557eae9ec
antonie <antonriziki@gmail.com>
2026-03-05 12:18:39 +0300

    vercel deployment prep

--- Nimiro_AI/Nimiro_AI/settings.py
+++ Nimiro_AI/Nimiro_AI/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1e3kb$#^!c-xnr!g7y)kr&u!d0r=3c1cfcg9*b*ae7*d448i@5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
