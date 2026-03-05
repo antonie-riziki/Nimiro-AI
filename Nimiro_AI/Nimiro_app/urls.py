f546b1b3f6ec617fc097ee8c640ba52834d30456
antonie <antonriziki@gmail.com>
2026-03-05 23:20:06 +0300

    feat: Implement authentication page and update homepage links to direct users to it.

--- Nimiro_AI/Nimiro_app/urls.py
+++ Nimiro_AI/Nimiro_app/urls.py
@@ -9,6 +9,10 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.landing_page, name='landing-page'),
+    path('auth/', views.auth_page, name='auth-page'),
+    path('dashboard/', views.dashboard_page, name='dashboard-page'),
+    path('farming_guide/', views.farming_guide, name='farming-guide'),
+    path('market_intelligence/', views.market_intelligence, name='market-intelligence'),
 
 ]
 
--- Nimiro_AI/Nimiro_app/views.py
+++ Nimiro_AI/Nimiro_app/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def landing_page(request):
     return render(request, 'index.html')
+
+def auth_page(request):
+    return render(request, 'auth.html')
+
+def dashboard_page(request):
+    return render(request, 'dashboard.html')
+
+def farming_guide(request):
+    return render(request, 'farming_guide.html')
+
+def market_intelligence(request):
+    return render(request, 'market_intelligence.html')
