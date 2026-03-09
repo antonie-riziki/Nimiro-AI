2a8268c8c806672bf440c2af4dc80b291d80e2c5
antonie <53366054+antonie-riziki@users.noreply.github.com>
2026-03-09 22:38:19 +0300

    Set DEBUG to False for production readiness

--- Nimiro_AI/Nimiro_AI/settings.py
+++ Nimiro_AI/Nimiro_AI/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1e3kb$#^!c-xnr!g7y)kr&u!d0r=3c1cfcg9*b*ae7*d448i@5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
